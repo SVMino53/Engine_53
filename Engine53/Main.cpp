4cf102376bc7720d62be9e907a500743ba3ebf15
Isak Forsberg <isak-forsberg@hotmail.se>
2024-01-02 17:37:37 +0100

    Implemented SDL2
    Implemented SDL2 and made a basic 'main' function.

--- Engine53/Main.cpp
+++ Engine53/Main.cpp
@@ -3,12 +3,22 @@
 // V 0.0, 2024-01-01
 
 #include <iostream>
+#include "SDL.h"
 
 
 
-int main()
+int main(int argc, char *args[])
 {
-	std::cout << "Hello world!";
+	SDL_Window* window = SDL_CreateWindow("Test", 500, 500, 160, 90, SDL_WINDOW_RESIZABLE);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+
+	while (true)
+	{
+		SDL_SetRenderDrawColor(renderer, 255, 100, 100, 255);
+		SDL_RenderClear(renderer);
+
+		SDL_RenderPresent(renderer);
+	}
 
 
 	return 0;
